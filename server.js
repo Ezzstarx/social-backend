2541fb56e990721ce82c70170b8ab0c25e5ad730
Harsh Upadhyay <harsh9756416665@gmail.com>
2026-02-26 10:05:46 +0530

    try

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get("/test", (req, res) => {
 });
 
 // ❌ REMOVE app.listen()
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
 // ✅ Export instead
 
 module.exports = app;
